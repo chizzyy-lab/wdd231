2abdf615147a43621e7d88bb9b418407ba0decce
chizzyy-lab <egwuatuchidera14@gmail.com>
2025-09-04 11:09:36 -0700

    Add files via upload

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,12 +1,11 @@
-const title = document.getElementById("myTitle"); // Get element by ID
-const parent = title.parentNode;                  // Move up to parent
-const firstChild = title.firstChild;              // Move down to first child
-const next = title.nextSibling;                   // Move sideways to next node
 
-/** @type {{ name: string, sayHello: () => void }} */
-const person = {
-  name: "Chidera",
-  sayHello: function () {
-    console.log("Hi! My name is", this.name);
-  }
-};
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menuToggle");
+  const navMenu = document.getElementById("navMenu");
+
+  menuToggle.addEventListener("click", function () {
+    navMenu.classList.toggle("active");
+  });
+});
